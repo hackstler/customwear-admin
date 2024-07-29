7247ae500d99ca89178d385659cda346aa7ed226
hackstler <sergio@hackstler.com>
2024-07-29 15:16:06 +0200

    fix name

--- src/subscribers/order-placed.ts
+++ src/subscribers/order-placed.ts
@@ -14,15 +14,19 @@ export default async function orderPlacedHandler({
   eventName,
   container,
 }: SubscriberArgs<OrderPlacedEvent>) {
-  console.log("🚀 ~ data:", data);
-  console.log("🚀 ~ eventName:", eventName);
   const orderService: OrderService = container.resolve("orderService");
   const sendGridService = container.resolve("sendgridService");
 
   const order = await orderService.retrieve(data.id, {
     relations: ["items", "customer"],
   });
-  console.log("🚀 ~ order:", order);
+
+  const formattedItems = order.items.map((item) => ({
+    nombre: item.title,
+    cantidad: item.quantity,
+    precio: (item.unit_price / 100).toFixed(2), // Ajustar según sea necesario
+  }));
+  console.log("🚀 ~ order.customer.first_name:", order.customer.first_name);
 
   await sendGridService.sendEmail({
     templateId: process.env.SENDGRID_ORDER_PLACED_ID,
@@ -30,12 +34,8 @@ export default async function orderPlacedHandler({
     to: order.email,
     dynamic_template_data: {
       nombre_cliente: order.customer.first_name,
-      articulos: order.items.map((item) => ({
-        nombre: item.title,
-        cantidad: item.quantity,
-        precio: item.unit_price / 100, // Ajustar según sea necesario
-      })),
-      total: order.total / 100, // Ajustar según sea necesario
+      articulos: formattedItems,
+      total: (order.total / 100).toFixed(2), // Ajustar según sea necesario
     },
   });
 }
